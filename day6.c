7e7287b60ff0cc8adede65a484149a19a76e94db
KyawZinThant <68962256+Maung-Kyaw@users.noreply.github.com>
2023-09-27 11:40:16 +0700

    if/else lecture

--- day6.c
+++ day6.c
@@ -4,7 +4,7 @@
 
 int main()
 {
-    printf("Hello world!\n\n\n");
+    /*printf("Hello world!\n\n\n");
 
     int a,b,c;
     float x1,x2;
@@ -13,12 +13,29 @@ int main()
     x1=(-b+sqrt(pow(b,2)-(4*a*c)))/(2*a);
     x2=(-b-sqrt(pow(b,2)-(4*a*c)))/(2*a);
     printf("x1=%.2f\n",x1);
-    printf("x2=%.2f",x2);
+    printf("x2=%.2f",x2);*/
+    int x,y,z;
+    int max;
+    int min;
+    printf("Input 3 numbers (x,y,z) :");
+    scanf("%d,%d,%d",&x,&y,&z);
+    if(x>y && x>z) max=x;
+    else if(y>z) max=y;
+    else max=z;
+    printf("max=%d\n",max);
+
+    if(x<y && x<z) min=x;
+    else if(y<z) min=y;
+    else min=z;
+    printf("min=%d\n",min);
+
 
 
 
 
 
     return 0;
+
+
 
 }
