979d01e3b238e9f60a5a633fc9d7930648994761
Kyaw Zin  Thant <68962256+Maung-Kyaw@users.noreply.github.com>
2023-11-01 11:22:04 +0700

    Array ( biggest & smallest )

--- main.c
+++ main.c
@@ -1,62 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#define Size 5
 
 int main()
 {
-    int a,b,c;
-    float x1,x2;
+    int i;
+    int a[Size]={};
 
-    printf("Enter your coefficieants:");
-    scanf("%d %d %d",&a,&b,&c);
-    puts("Roots of the equation");
-
-    switch (a){
-        case 1:printf("x^2");break;
-        case 0:break;
-        case -1:printf("-x^2");break;
-        default: printf("%dx^2",a);
+    for(i=0;i<Size;i++){
+        printf("Input numbers %d : ",i);
+        scanf("%d",&a[i]);
     }
-    /*switch (b>0){
-        case 1:printf("+");break;
-    }*/
-
-    if(a!=0 && b>0) printf("+");
-
-    switch (b){
-        case 1:printf("x");break;
-        case 0:break;
-        case -1:printf("-x");break;
-        default: printf("%dx",b);
+    printf("\n");
+    for(i=0;i<Size;i++){
+        printf("%3d",a[i]);
     }
-    if((a!=0 || b!=0 )&& c>0) printf("+");
-
-    switch (c){
-        case 1:printf("1");break;
-        case 0:break;
-        case -1:printf("-1");break;
-        default: printf("%d",c);
+    int big=a[0];
+    int small=a[0];
+    for(i=1;i<Size;i++){
+        if(big<a[i]) big=a[i];
+        if(small>a[i]) small=a[i];
     }
-    if(a==0 && b==0 && c==0 ){
-        puts("");
-    }
-    else puts("=0");
+    printf("\n\nThe biggest number is %d",big);
+    printf("\n\nThe smallest number is %d\n",small);
 
-    float sq=b*b-4*a*c;
 
-    if(a==0){
-        if(b!=0) printf("The answer is %.2f\n",-(float)c/b);
-        else printf("No solution");
 
-    }
-    else{
-        if(sq==0) printf("The answer is %.2f\n",(float)-b/2*a);
-        else if(sq<0) printf("No solution!");
-        else{
-            x1=(-b+sqrt(b*b-4*a*c))/(2*a);
-            x2=(-b-sqrt(b*b-4*a*c))/(2*a);
-            printf("The value of x1 : %.2f\n",x1);
-            printf("The value of x2 : %.2f\n",x2);
-        }
-    }
+    return 0;
 }
